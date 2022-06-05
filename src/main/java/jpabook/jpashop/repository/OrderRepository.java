aa1c03fe9173590e7659eb0510edec21f588573e
Minkyung <mminkyung.k@gmail.com>
2022-06-05 17:08:37 +0900

    Work on order search

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -19,5 +20,13 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {}
+    public List<Order> findAll(OrderSearch orderSearch) {
+        // JPQL
+        return em.createQuery("select o from Order o join o.member m where o.status = :status and m.name like :name", Order.class)
+                .setParameter("status", orderSearch.getOrderStatus())
+                .setParameter("name", orderSearch.getMemberName())
+//                .setFirstResult(100) paging 처리
+                .setMaxResults(1000)
+                .getResultList();
+    }
 }
